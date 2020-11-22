f0fc6ee3ccde088306d41cbd83fed75e19571a87
Manoj Chennareddy <52932901+ManojChenna@users.noreply.github.com>
2020-11-22 22:27:59 +0530

    VideoDetail

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import Youtube from "../apis/Youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onEnterSubmit = async (searchText) => {
     const resp = await Youtube.get("/search", {
@@ -14,11 +15,19 @@ class App extends React.Component {
     });
     this.setState({ videos: resp.data.items });
   };
+
+  selectedVideo = (video) => {
+    this.setState({ selectedVideo: video });
+  };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onEnterSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onSelectVideo={this.selectedVideo}
+        />
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Videoitem from "./VideoItem";
 
-const VideoList = (props) => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ({ videos, onSelectVideo }) => {
+  const renderedVideos = videos.map((video) => {
+    return (
+      <Videoitem
+        video={video}
+        selectedVideo={onSelectVideo}
+        key={video.id.videoId}
+      />
+    );
+  });
+  return <div className="ui relaxed divided list">{renderedVideos}</div>;
 };
 
 export default VideoList;
